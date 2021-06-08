3a2683fa6fe7deb91d17c62111478ae5d7a2a124
Shubham Kumar <kumar.shubham5504@gmail.com>
2021-06-08 00:45:30 -0700

    nested dictionary for holding locker data

--- SRVUSD-Lockers/assignment.py
+++ SRVUSD-Lockers/assignment.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class Lockers:
     # example of DV attributes
     # attributes = [
@@ -5,18 +7,18 @@ class Lockers:
     #     ['1', '2'], (level)
     #     ['T', 'B'] (row)
     # ]
-    def __init__(self, attributes):
+    def __init__(self, spreadsheet, attributes):
         self.attributes = attributes
+        self.s = spreadsheet
 
         def f(l, d_last):
-            d = {}
-            for i in l:
-                d[i] = d_last
-            return d.copy()
+          d = {}
+          for i in l:
+            d[i] = deepcopy(d_last)
+          return d
 
-        d_last = {i:[] for i in attributes[-1]}
+        d_last = {i:list() for i in attributes[-1]}
         for i in range(len(attributes)-2, -1, -1):
-            d_last = f(attributes[i], d_last.copy())
+            d_last = f(attributes[i], d_last)
 
-        self.organized_lockers = d_last.copy()
-        d_last = None
+        self.d = deepcopy(d_last)
--- SRVUSD-Lockers/main.py
+++ SRVUSD-Lockers/main.py
@@ -1,10 +1,18 @@
 from match import Match
 from spreadsheet import Spreadsheet
+from assignment import Lockers
 import config
+
+dv_attributes = [
+    ['1000', '2000', '3000', '4000'],
+    ['1', '2'],
+    ['T', 'B']
+]
 
 # creating objects
 s = Spreadsheet(config.TEST_SPREADSHEET_ID)
 m = Match(s)
+l = Lockers(s, dv_attributes)
 
 # testing class structure
 partnerships = m.get_partners()
