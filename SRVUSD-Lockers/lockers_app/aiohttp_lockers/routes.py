ffb133cfce4fdf7299fc18dc052122b7fdb6046d
Shubham Kumar <kumar.shubham5504@gmail.com>
2021-06-09 14:54:55 -0700

    url names

--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/routes.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/routes.py
@@ -1,5 +1,6 @@
+from aiohttp import web
 from views import index, student
 
 def setup_routes(app):
-    app.router.add_get('/', index)
-    app.router.add_get('/student', student)
+    app.router.add_get('/', index, name='index')
+    app.router.add_get('/student', student, name='student')
