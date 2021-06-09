6138255ec697ed6a0f55267c3942a650a42bc745
Shubham Kumar <kumar.shubham5504@gmail.com>
2021-06-09 15:58:01 -0700

    Static Files Handled

--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/main.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/main.py
@@ -8,7 +8,8 @@ from db import close_pg, init_pg
 app = web.Application()
 app['config'] = config
 aiohttp_jinja2.setup(app,
-    loader=jinja2.FileSystemLoader('aiohttp_lockers/templates'))
+    loader=jinja2.FileSystemLoader('templates/'))
+app.add_routes([web.static('/static', 'static/')])
 setup_routes(app)
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
