3305b471b8a5d2fb2d3c44e7f93037d308b99d7e
Shubham Kumar <kumar.shubham5504@gmail.com>
2021-06-09 11:23:11 -0700

    Working Jinja2 proto with base template

--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/main.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/main.py
@@ -1,6 +1,10 @@
 from aiohttp import web
 from routes import setup_routes
+import aiohttp_jinja2
+import jinja2
 
 app = web.Application()
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader('templates/'))
 setup_routes(app)
 web.run_app(app)
--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/views.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/views.py
@@ -1,4 +1,7 @@
 from aiohttp import web
+import aiohttp_jinja2
 
+@aiohttp_jinja2.template('nav_base.html')
 async def index(request):
-  return web.Response(text='Hello Aiohttp!')
+  # return web.Response(text='Hello Aiohttp!')
+  return {}
