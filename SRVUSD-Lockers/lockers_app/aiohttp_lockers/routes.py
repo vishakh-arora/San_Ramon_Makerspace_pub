1ade0876a6c85a52146090b13a49c880f412df53
Vishakh Arora <vishakh.arora29@gmail.com>
2021-06-13 04:01:23 +0000

    Domain set up, google signin working, need to change domain validation to use db lookup

--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/routes.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/routes.py
@@ -1,7 +1,9 @@
 from aiohttp import web
-from views import index, student, admin
+from views import *
 
 def setup_routes(app):
     app.router.add_get('/', index, name='index')
     app.router.add_get('/student', student, name='student')
     app.router.add_get('/admin', admin, name='admin')
+    app.router.add_get('/dashboard', dashboard, name='dashboard')
+    app.router.add_post('/tokensignin', tokensignin, name='tokensignin')
--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/views.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/views.py
@@ -1,6 +1,14 @@
 from aiohttp import web
 import aiohttp_jinja2
-import db
+import db, asyncio
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+CLIENT_ID = '745601090768-kosoi5uc466i9ns0unssv5h6v8ilk0a8.apps.googleusercontent.com'
+
+async def dashboard(request):
+    location = request.app.router['admin'].url_for()
+    raise web.HTTPFound(location=location)
 
 @aiohttp_jinja2.template('index.html')
 async def index(request):
@@ -18,3 +26,30 @@ async def student(request):
 @aiohttp_jinja2.template('admin.html')
 async def admin(request):
     return {}
+
+@asyncio.coroutine
+def tokensignin(request):
+    data = yield from request.post()
+    token = data['idtoken']
+    try:
+        # Specify the CLIENT_ID of the app that accesses the backend:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+        print(idinfo)
+
+        # If auth request is from a G Suite domain:
+        domain = idinfo.get('hd')
+        #print(idinfo['hd'])
+        if domain != None:
+        #****Change validation to db lookup****
+            if 'srvusd' not in domain:
+                raise ValueError('Wrong hosted domain.')
+        else:
+            raise ValueError('Wrong hosted domain.')
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        location = request.app.router['admin'].url_for()
+        raise web.HTTPFound(location=location)
+    except ValueError:
+        # Invalid token
+        pass
