6277ebebb286a382fd5c851c317d5f2358773535
vishakh-arora <vishakh.arora29@gmail.com>
2021-06-15 00:40:39 -0700

    Updated db structure and added comments so everything's clear, added packages to requirements.txt

--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/db.py
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/db.py
@@ -6,29 +6,9 @@ from sqlalchemy import (
 from sqlalchemy_json import mutable_json_type
 from sqlalchemy.dialects.postgresql import JSONB
 
-__all__ = ['student', 'admin', 'school', 'partner', 'locker', 'organization']
+__all__ = ['student', 'admin', 'school', 'preference', 'locker', 'organization', 'assignment']
 
 meta = MetaData()
-
-# question = Table(
-#     'question', meta,
-#
-#     Column('id', Integer, primary_key=True),
-#     Column('question_text', String(200), nullable=False),
-#     Column('pub_date', Date, nullable=False)
-# )
-#
-# choice = Table(
-#     'choice', meta,
-#
-#     Column('id', Integer, primary_key=True),
-#     Column('choice_text', String(200), nullable=False),
-#     Column('votes', Integer, server_default="0", nullable=False),
-#
-#     Column('question_id',
-#            Integer,
-#            ForeignKey('question.id', ondelete='CASCADE'))
-# )
 
 student = Table(
     'student', meta,
@@ -39,10 +19,12 @@ student = Table(
     Column('last_name', String(100), nullable=False),
     Column('school_id', Integer, ForeignKey('school.id', ondelete='CASCADE'), nullable=False),
     Column('grade', Integer, nullable=False),
-    Column('submit_time', DateTime(timezone='UTC')),
+
     #Column('assignment', mutable_json_type(dbtype=JSONB, nested=True))
 )
 
+# Store every school administrator using this website
+# Should be manually pre-populated
 admin = Table(
     'admin', meta,
 
@@ -50,10 +32,11 @@ admin = Table(
     Column('email', String(100), nullable=False),
     Column('prefix', String(7), nullable=False),
     Column('last_name', String(100), nullable=False),
-    Column('school_id', Integer, ForeignKey('school.id', ondelete='CASCADE'), nullable=False),
-    #Column('responses', mutable_json_type(dbtype=JSONB, nested=True))
+    Column('school_id', Integer, ForeignKey('school.id', ondelete='CASCADE'), nullable=False)
 )
 
+# Store every school using this website
+# Should be manually pre-populated
 school = Table(
     'school', meta,
 
@@ -61,46 +44,66 @@ school = Table(
     Column('name', String(100), nullable=False),
 )
 
-partner = Table(
-    'partner', meta,
+# Store student preferences for their partner/locker
+# Should be populated upon a student's submission of the form
+preference = Table(
+    'preference', meta,
+    Column('submit_time', DateTime(timezone='UTC'), nullable=False), # Store a timezone aware datetime object storing the current time
     Column('student_id', Integer, ForeignKey('student.id', ondelete='CASCADE'), primary_key=True, nullable=False),
     Column('partner_id', Integer, ForeignKey('student.id', ondelete='CASCADE'), primary_key=True, nullable=False),
-    Column('preference', Integer, nullable=False),
+    Column('partner_rank', Integer, nullable=False), # 1, 2, or 3 depending on how the student ranked this partner
+    Column('locker_pref', Integer, ForeignKey('organization.id', ondelete='CASCADE'), nullable=False) # Points to one of the 20-30 possible
+                                                                                                      #  organizations stored in the organizations table
 )
-# Vishakh | Shubham 0
-# Vishakh | Chaitu 1
-# Vishakh | Shlok 2
 
+# Store every locker in every school
+# Should be populated from lockers spreadsheet
 locker = Table(
     'locker', meta,
 
     Column('id', Integer, primary_key=True),
-    Column('number', String(32), nullable=False), #1234
-    Column('school_id', Integer, ForeignKey('school.id', ondelete='CASCADE'), nullable=False), #DVHS
+    Column('number', String(32), nullable=False),
+    Column('school_id', Integer, ForeignKey('school.id', ondelete='CASCADE'), nullable=False),
     Column('combination', String(10)),
-    Column('organization', Integer, ForeignKey('organization.id', ondelete='CASCADE'), nullable=False), #id
-    #1000 | 2 | 2 | 1
+    Column('organization', Integer, ForeignKey('organization.id', ondelete='CASCADE'), nullable=False)
 )
-#
+
+
+# Store every possible organization as values
+# Should be populated from lockers spreadsheet
 organization = Table(
     'organization', meta,
 
     Column('id', Integer, primary_key=True),
     Column('school_id', Integer, ForeignKey('school.id', ondelete='CASCADE'), nullable=False),
-    Column('hierarchy_1', String(32)), #1000, 2000, 3000, 4000
-    Column('hierarchy_2', String(32)), # Floor
-    Column('hierarchy_3', String(32)), # Level
+    Column('hierarchy_1', String(32)), #1000, 2000, 3000, 4000 (building)
+    Column('hierarchy_2', String(32)), # 1,2 (Floor)
+    Column('hierarchy_3', String(32)), # T, B (Level)
     Column('hierarchy_4', String(32)),
-    Column('hierarchy_5', String(32)),
-    Column('hierarchy_6', String(32)),
-    Column('hierarchy_7', String(32)),
-    Column('hierarchy_8', String(32)),
-    Column('hierarchy_9', String(32)),
-    Column('hierarchy_10', String(32))
+    Column('hierarchy_5', String(32))
 )
-# locker # | combination (optional) | hierarchy (largest --> smallest level)
-# 1 | 23 | building | floor | bay | level | row
-# building | level | row
+# Example of organization table:
+# 1 | 23 | 1000 | 2 | T
+# 2 | 23 | 1000 | 2 | B
+# 3 | 23 | 1000 | 1 | T
+# 4 | 23 | 1000 | 1 | B
+# 5 | 23 | 2000 | 2 | T
+# ...
+# There will only be 20-30 rows like this (ex. 4 buildings * 2 floors * 3 levels = 24),
+# there will be many lockers belonging to each row though.
+# This makes it easy to look up all lockers belonging to a specific org - simply look up all lockers that have
+# a specific org id.
+
+# Store final outcomes of partner/locker assignment
+# Should be populated from preassignments spreadsheet
+assignment = Table(
+    'assignment', meta,
+    Column('student_id', Integer, ForeignKey('student.id', ondelete='CASCADE'), primary_key=True, nullable=False),
+    Column('partner_id', Integer, ForeignKey('student.id', ondelete='CASCADE')),
+    Column('status', String(6), nullable=False), # either 'ASSIGN' or 'MATCH' depending on if the student got any of their preferences or not
+    Column('locker_id', Integer, ForeignKey('locker.id', ondelete='CASCADE'), nullable=False)
+)
+
 async def init_pg(app):
     conf = app['config']['postgres']
     engine = await aiopg.sa.create_engine(
