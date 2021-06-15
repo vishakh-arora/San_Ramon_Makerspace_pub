803f5a916d9e11907b2a2838c3068ad695dbe879
Shubham Kumar <shubhamkumar@srvusd-lunch.com>
2021-06-15 19:56:04 +0000

    Not integrated with OAuth2

--- SRVUSD-Lockers/lockers_app/aiohttp_lockers/static/js/main.js
+++ SRVUSD-Lockers/lockers_app/aiohttp_lockers/static/js/main.js
@@ -3,7 +3,7 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://lockermatch.com/login', true);
-    xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     // xhr.onload = function() {
     //   console.log('Signed in as: ' + xhr.responseText);
     // };
